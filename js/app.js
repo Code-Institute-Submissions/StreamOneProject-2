6b4ed8a806432ec398b651e560e072bb887afc7f
Steven Patterson <sbarrypatterson@gmail.com>
2018-08-09 13:47:43 -0400

    Homepage 50% finished

---
+++ js/app.js
@@ -0,0 +1,22 @@
+angular.module('TPBfanpage', ['ngRoute', 'RouteControllers', 'angular-storage']);
+
+angular.module('TPBfanpage').config(function($locationProvider, $routeProvider){
+	$locationProvider.html5Mode(true);  //Enable href routing w/o hashes
+
+	$routeProvider.when('/', {
+		templateUrl: 'templates/home.html',
+		controller: 'HomeController'
+	})
+	.when('/trivia', {
+		templateUrl: 'templates/trivia.html',
+		controller: 'TriviaController'
+	})
+	.when('/tours', {
+		templateUrl: 'templates/tours.html',
+		controller: 'ToursController'
+	})
+	.when('/fanMail', {
+		templateUrl: 'templates/fanMail.html',
+		controller: 'MailController'
+	});
+});
